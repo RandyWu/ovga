79a40f286f8c06edd12b08690caba3bcbd96db0c
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-01 14:45:37 -0500

    added init file

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, login_user, login_required
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+#   Setting up path of Config file
+app.config.from_object('config.Conf')
+
+#   TODO: 404 page
+# @app.errorhandler(404)
+# def page_not_fount(error):
+#     pass
+
+#   Init Model
+db = SQLAlchemy(app)
+db.create_all()
+
+
+#   Loading Model Classes and Controller Classes
+# from app.model import models
+# from app.controller import *
--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
@@ -11,4 +9,4 @@ def scores():
     return render_template("scores.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
