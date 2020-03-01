f022cc81e7d829a9d9d612f20739a3209dd5a646
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-01 14:01:27 -0500

    Added scoring template and config file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/scores')
+def scores():
+    return render_template("scores.html")
 
 if __name__ == '__main__':
     app.run()
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Conf(object):
+    DEBUG = True
+    DEVELOPMENT = True
+    SITE_NAME = "A Test Site Name"  #   It will affect the whole website
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = "mysql://admin:Vancouver2010@155.138.138.91:23306/PublicDB"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
