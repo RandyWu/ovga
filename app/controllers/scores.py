3f863367d35dac6f0c6c0c6e8d5860cf368b3f5a
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-10 22:42:51 -0400

    Score page now populates based on division and players

--- app/controllers/scores.py
+++ app/controllers/scores.py
@@ -4,9 +4,31 @@ from flask import Flask, render_template
 
 @app.route('/scores')
 def scores():
-    player_scores = Score.query.filter_by(PlayerID=1).all()
-    course = Course.query.filter_by(CourseID=1).first()
-    holes = Hole.query.filter_by(CourseID=1).order_by(Hole.Hole_Num.asc())
+    event_id = 1
+    course_id = 1
+
+    all_players = PlayerDivision.query.filter_by(Event_Id=event_id).all()
+    all_divisions = Division.query.filter_by(EventID=event_id).distinct().all()
+
+    page_info = {}
+    score_totals = {}
+    for divisions in all_divisions:
+        page_info[divisions.DivisionID] = {}
+        # page_info[divisions.DivisionID].update({})
+
+    for player in all_players:
+        page_info[player.Division_Id].update({player.Player_Id:{}})
+        player_scores = Score.query.filter_by(PlayerID=player.Player_Id, EventID=event_id).all()
+
+        for score in player_scores:
+            page_info[player.Division_Id][player.Player_Id].update({score.HoleID : score.Strokes})
+
+        score_totals.update({player.Player_Id : sum(page_info[player.Division_Id][player.Player_Id].values())})
+        
+
+    # course = Course.query.filter_by(CourseID=course_id).first()
+
+    holes = Hole.query.filter_by(CourseID=course_id).order_by(Hole.Hole_Num.asc())
     
     num_of_holes = 0
     for entry in holes:
@@ -23,12 +45,11 @@ def scores():
     player_total_score = sum(score_list)
 
     return render_template(
-            "scores.html", 
-            holes = holes, 
-            course = course, 
-            num_of_holes = num_of_holes, 
-            par_list = par_list, 
-            par_total = par_total, 
-            player_scores = player_scores,
-            player_total_score = player_total_score
-            )
+        "scores.html", 
+        holes = holes, 
+        page_info = page_info, 
+        num_of_holes = num_of_holes, 
+        par_list = par_list, 
+        par_total = par_total,
+        score_totals = score_totals
+        )
