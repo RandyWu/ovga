ebe72c2efe127425c4f65ddf687e53ccafb30753
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-15 13:06:18 -0400

    Add event page functionality implemented

--- app/controllers/addevent.py
+++ app/controllers/addevent.py
@@ -1,6 +1,20 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from app.model.model import db,Venue,Event
 
-@app.route('/addevent')
+@app.route('/addevent', methods=['POST', 'GET'])
 def addevent():
-    return render_template("addevent.html")
+    venue_list = Venue.query.all()
+    rawr = request.form
+    if request.method == 'POST' and request.form["submit"]:
+
+        selected_venue = int(request.form.get("selectedVenue"))
+        venue_address = Venue.query.filter_by(VenueId=selected_venue).first().Address
+        event_name = request.form.get("event_name")
+        event_date = request.form.get("selected_date")
+
+        new_event = Event(Venue_Id=selected_venue,Name=event_name,Address=venue_address,Date=event_date)
+        db.session.add(new_event)
+        db.session.commit()
+
+    return render_template("addevent.html", venue_list = venue_list)
