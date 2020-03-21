757bf0c7575ac0c27f8c2b7874ffe0aa6d1434b3
omea0024 <32466334+omea0024@users.noreply.github.com>
2020-03-21 11:52:51 -0400

    login progress

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -1,7 +1,21 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
+from app.controllers.forms import LoginForm
+from app import login
+from flask_login import current_user, login_user
+from app.model.model import User
 
-@app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('index'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
