9533d0aa81741c9d78e3194563ea58b135298de1
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-26 15:12:01 -0400

    Edit Events feature working

--- app/controllers/delevent.py
+++ app/controllers/delevent.py
@@ -5,9 +5,22 @@ from app.model.model import db,Event,Player,PlayerDivision,Score
 @app.route('/delevent', methods=['POST', 'GET'])
 def delevent():
     if request.method == 'POST' and request.form["submit"]:
-        event_id = request.form.get('event_select')
+        event_id = int(request.form.get('event_select'))
 
-        removed_event = PlayerDivision.query.filter_by(Event_Id=removed_event).all()
+        removed_event = Event.query.filter_by(EventId=event_id).first()
+        removed_divisions = PlayerDivision.query.filter_by(Event_Id=event_id).all()
+        removed_scores = Score.query.filter_by(EventID=event_id).all()
+
+        if removed_event:
+            db.session.delete(removed_event)
+        if removed_divisions:
+            db.session.delete(removed_scores)
+        if removed_scores:
+            for score in removed_scores:
+                db.session.delete(score)
+
+        rawr = request.form
+        db.session.commit()
 
     event_list = Event.query.all()
     return render_template("delevent.html", event_list=event_list)
