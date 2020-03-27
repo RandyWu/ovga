c7b345e0ed64a9d06cdc7bf18a347ed746fdae82
Vikram Kamalakannan <43158970+kama0070@users.noreply.github.com>
2020-03-27 16:30:37 -0400

    editpersonal, editplayerinfo - Python Complete

--- app/controllers/editpersonal.py
+++ app/controllers/editpersonal.py
@@ -1,6 +1,22 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
+from app.model.model import db, Admin, User
 
-@app.route('/editpersonal')
+@app.route('/editpersonal', methods=['POST', 'GET'])
 def editpersonal():
-    return render_template("editpersonal.html")
+    session['admin_id'] = 1
+    alert = "none"
+    
+    if request.method == 'POST' and request.form.get('submit') == 'submit':    
+        admin_info = Admin.query.filter_by(Admin_Id=session['admin_id']).first()  
+        admin_info.Email = request.form['my_email']
+        admin_info.Password = request.form['my_passwd']
+        db.session.commit()
+        alert = "block";
+        
+    admin_info = Admin.query.filter_by(Admin_Id=session['admin_id']).first()
+    my_id = admin_info.Admin_Id
+    my_email = admin_info.Email
+    my_passwd = admin_info.Password
+        
+    return render_template("editpersonal.html", my_id=my_id, my_email=my_email, my_passwd=my_passwd, alert=alert)
--- app/controllers/editplayerinfo.py
+++ app/controllers/editplayerinfo.py
@@ -1,6 +1,37 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify
+from app.model.model import db, Player
 
-@app.route('/editplayerinfo')
+@app.route('/editplayerinfo', methods=['POST', 'GET'])
 def editplayerinfo():
-    return render_template("editplayerinfo.html")
+    players_list = Player.query.all()
+    
+    if len(players_list) != 0:
+        success = 'TRUE'
+    else:
+        success = 'FALSE'
+        
+    return render_template("editplayerinfo.html", players_list = players_list, success = success)
+
+@app.route('/getplayerinfo', methods=['POST', 'GET'])  
+def getplayerinfo():
+    if request.form['player_select'] != '-1':
+        player_id = request.form['player_select']
+        selected_player = Player.query.filter_by(PlayerId=player_id).first()
+        player_name = selected_player.Name
+        player_email = selected_player.Email
+        player_passwd = selected_player.Password
+        
+        return jsonify({'player_name':player_name, 'player_email':player_email, 'player_passwd':player_passwd})
+
+@app.route('/saveplayerinfo', methods=['POST', 'GET'])
+def saveplayerinfo():
+    players_list = Player.query.all()  
+    player_id = int(request.form['player_select'])
+    player = Player.query.filter_by(PlayerId=player_id).first()
+    player.Name = request.form['player_name']
+    player.Password = request.form['player_passwd']
+    player.Email = request.form['player_email']
+    db.session.commit()
+    
+    return render_template("editplayerinfo.html", players_list = players_list)
