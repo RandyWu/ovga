fd38923205e4a4c3d11d8688a9a04a7ed8d79ac4
Randy Wu <RandyWu@users.noreply.github.com>
2020-03-26 20:49:16 -0400

    Admin Venue functionality complete

--- app/controllers/addvenue.py
+++ app/controllers/addvenue.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask,session,render_template,request
+from app.model.model import db,Venue
 
-@app.route('/addvenue')
+@app.route('/addvenue', methods=['POST','GET'])
 def addvenue():
+    if request.method == 'POST':
+        venue_name = request.form.get("venue_name")
+        venue_address = request.form.get("venue_address")
+
+        new_venue = Venue(Name=venue_name,Address=venue_address)
+        db.session.add(new_venue)
+        db.session.commit()
+    
     return render_template("addvenue.html")
--- app/controllers/upddelvenue.py
+++ app/controllers/upddelvenue.py
@@ -1,6 +1,31 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask,session,render_template,request
+from app.model.model import db,Venue
 
-@app.route('/upddelvenue')
+@app.route('/upddelvenue', methods=['POST', 'GET'])
 def upddelvenue():
-    return render_template("upddelvenue.html")
+    if request.method == 'POST':
+        action = request.form.get('submit')
+        selected_venue = int(request.form.get('select_venue'))
+        venue = Venue.query.filter_by(VenueId=selected_venue).first()
+
+        if action == 'update':
+            venue_name = request.form.get('venue_name')
+            venue_address = request.form.get('venue_address')
+
+            venue.Name = venue_name
+            venue.Address = venue_address
+            db.session.commit()
+            
+            venue_list = Venue.query.all()
+            return render_template("upddelvenue.html", venue_list=venue_list)
+
+        if action == 'remove':
+            db.session.delete(venue)
+            db.session.commit()
+
+            venue_list = Venue.query.all()
+            return render_template("upddelvenue.html", venue_list=venue_list)
+
+    venue_list = Venue.query.all()
+    return render_template("upddelvenue.html", venue_list=venue_list)
