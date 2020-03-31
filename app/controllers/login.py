8af13632982cf1a16446505c37e5ac20c8007b33
omea0024 <32466334+omea0024@users.noreply.github.com>
2020-03-30 22:35:18 -0400

    login errors

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -1,21 +1,23 @@
 from app import app
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, request, url_for
 from app.controllers.forms import LoginForm
 from app import login
 from flask_login import current_user, login_user
-from app.model.model import User
+from app.model.model import Admin, Player
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
-    
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
+        admin = Admin.query.filter_by(Email=form.email.data).first()
+        player = Player.query.filter_by(Email=form.email.data).first()
+        if (admin or player):
+            return "hi"
+        else:
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        # if user is None or not user.check_password(form.password.data):
+        #     flash('Invalid username or password')
+        #     
+        # return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
