44392e476f8c4d99d57ed666a92c6230256dbeed
Randy Wu <RandyWu@users.noreply.github.com>
2020-04-01 11:13:36 -0400

    add course functionality complete

--- app/controllers/addcourse.py
+++ app/controllers/addcourse.py
@@ -1,6 +1,22 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from app.model.model import db,Venue,Course
 
-@app.route('/addcourse')
+#TODO: Confirmation message on successful submit
+#TODO: Form validation for course_name. No special characters
+#TODO: Form security
+#TODO: Form validation for hole_num. Numbers only
+
+@app.route('/addcourse', methods=['POST','GET'])
 def addcourse():
-    return render_template("addcourse.html")
+    if request.method == 'POST' and request.form["submit"]:
+        venue_id = int(request.form.get('venue_select'))
+        course_name = request.form.get('course_name')
+        hole_num = int(request.form.get('hole_num'))
+
+        new_course = Course(VenueID=venue_id,CourseName=course_name,Num_Holes=hole_num)
+        db.session.add(new_course)
+        db.session.commit()
+
+    venue_list = Venue.query.all()
+    return render_template("addcourse.html", venue_list=venue_list)
