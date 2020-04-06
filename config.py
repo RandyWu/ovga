043028cd24fd04b6400c5fd5bf0bf5d64f95ea01
Randy Wu <RandyWu@users.noreply.github.com>
2020-04-06 10:34:42 -0400

    modified config db info

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Conf(object):
     DEVELOPMENT = True
     SITE_NAME = "OVGA Scoring"  #   It will affect the whole website
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = "mysql://admin:Vancouver2010@155.138.138.91:23306/PublicDB"
+    SQLALCHEMY_DATABASE_URI = "mysql://admin:Vancouver2010@15.222.176.252:23306/PublicDB"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
