4471431e64381ac634b1635366bb301828bd2ed1
Randy Wu <RandyWu@users.noreply.github.com>
2020-04-06 14:40:50 -0400

    Edit player personal info functionality added

--- app/controllers/editevent_player.py
+++ app/controllers/editevent_player.py
@@ -2,26 +2,6 @@ from app import app
 from flask import Flask,session,render_template,request
 from app.model.model import db,Event,Player,PlayerDivision
 
-@app.route('/editevent_player/registered_users', methods=['POST', 'GET'])
-def get_registered():
-    action = request.form.get("action")
-    selected_event = int(session['selected_event'])
-    if action == "add":
-        player_list = Player.query\
-            .join(PlayerDivision, Player.PlayerId==PlayerDivision.Player_Id)\
-            .filter((PlayerDivision.Event_Id != selected_event) | (PlayerDivision.Event_Id == None)).all()
-        success = 'TRUE'
-    elif action == "remove":
-        player_list = Player.query\
-            .join(PlayerDivision, Player.PlayerId==PlayerDivision.Player_Id)\
-            .filter_by(Event_Id=selected_event).all()
-        success = 'TRUE'
-    else:
-        success = 'FALSE'
-
-    return render_template("get_players.html", player_list=player_list, success=success)
-        
-
 @app.route('/editevent_player', methods=['POST', 'GET'])
 def editevent_player():
     if session['selected_event']:
@@ -59,3 +39,50 @@ def editevent_player():
                 return render_template("editevent_player.html", registered_players=registered_players)
 
     return render_template("editevent_player.html", registered_players=registered_players)
+
+@app.route('/editevent_player/registered_users', methods=['POST', 'GET'])
+def get_registered():
+    action = request.form.get("action")
+    selected_event = int(session['selected_event'])
+    if action == "add":
+        # Failed subquery attempts
+        # player_list = Player.query\
+        #     .join(PlayerDivision, Player.PlayerId==PlayerDivision.Player_Id)\
+        #     .filter((PlayerDivision.Event_Id != selected_event) | (PlayerDivision.Event_Id == None)).all()
+        # subquery = PlayerDivision.query.filter(PlayerDivision.Player_Id)
+        # player_list = Player.query.filter(Player.PlayerId.notin_(subquery)).all()
+
+        player_list = Player.query.filter(
+            ~PlayerDivision.query.filter(Player.PlayerId == PlayerDivision.Player_Id)
+            .exists()
+        ).all()
+        
+        if player_list:
+            success = 'TRUE'
+        else:
+            success = 'FALSE'
+
+    elif action == "remove":
+        player_list = Player.query\
+            .join(PlayerDivision, Player.PlayerId==PlayerDivision.Player_Id)\
+            .filter_by(Event_Id=selected_event).all()
+        if player_list:
+            success = 'TRUE'
+        else:
+            success = 'FALSE'
+    else:
+        success = 'FALSE'
+
+    return render_template("get_players.html", player_list=player_list, success=success)
+
+# TODO: Figure out a way to add groups to events
+# @app.route('/editevent_player/get_groups', methods=['POST', 'GET'])
+# def get_groups():
+#     action = request.form.get("action")
+#     selected_event = int(session['selected_event'])
+
+#     if action == "add":
+#         groups = PlayerDivision.query.filter_by()
+#     elif action == "remove":
+#     else:
+#         return None
--- app/controllers/editplayerpersonal.py
+++ app/controllers/editplayerpersonal.py
@@ -1,6 +1,25 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask,session,render_template,request
+from app.model.model import db, Player
 
-@app.route('/editplayerpersonal')
+# TODO: Confirmation message shows when successfully sent
+@app.route('/editplayerpersonal', methods=['POST', 'GET'])
 def editplayerpersonal():
-    return render_template("editplayerpersonal.html")
+    #Change player_id to logged in user from session in the future
+    player_id = 3
+
+    if request.method == 'POST' and request.form['submit']:
+        player_name = request.form.get('player_name')
+        player_email = request.form.get('player_email')
+        player_passwd = request.form.get('player_email')
+
+        player = Player.query.filter_by(PlayerId=player_id).first()
+        player.Name = player_name
+        player.Email = player_email
+        #TODO : encrypt with sha1
+        player.Password = player_passwd
+
+        db.session.commit()
+
+    player = Player.query.filter_by(PlayerId=player_id).first()
+    return render_template("editplayerpersonal.html",player=player)
