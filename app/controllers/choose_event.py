e49b4dd7c2861c5207aa752cd70db0774d7f6103
Randy Wu <RandyWu@users.noreply.github.com>
2020-04-08 11:53:50 -0400

    Player view scores functionality complete

--- app/controllers/choose_event.py
+++ app/controllers/choose_event.py
@@ -1,6 +1,24 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask,session,render_template,request,redirect,url_for
+from app.model.model import db,Event,PlayerDivision
+from flask_login import current_user, login_user
 
-@app.route('/choose_event')
+@app.route('/choose_event', methods = ['POST','GET'])
 def choose_event():
-    return render_template("choose_event.html")
+    user = current_user.UserId
+
+    if request.method == 'POST' and request.form['submit']:
+        selected_event = int(request.form.get('event_select'))
+        return redirect(url_for('scores'))
+
+    registered_events = PlayerDivision.query.filter_by(Player_Id=user).all()
+    registered_players = PlayerDivision.query.filter_by(Player_Id=user).distinct().all()
+
+    events = []
+    for event in registered_events:
+        e = Event.query.filter_by(EventId=event.Event_Id).first()
+        event_id = e.EventId
+        event_info = e.Name + " - " + e.Address 
+        events.append((event_id,event_info))
+
+    return render_template("choose_event.html", events=events)
