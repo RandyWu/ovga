b389a781a7a1c6a3ef047b464ce84a89c0ad1147
Randy Wu <RandyWu@users.noreply.github.com>
2020-04-21 20:12:10 -0400

    events error checking

--- app/controllers/scores.py
+++ app/controllers/scores.py
@@ -5,60 +5,66 @@ from app.model.model import *
 
 @app.route('/scores/<event_id>')
 def scores(event_id):
-    event = Event.query.filter_by(EventId=event_id).first()
-    course_id = event.CourseID
-    event_name = event.Name
-    event_address = event.Address
-    venue_name = Venue.query.filter_by(VenueId=event.Venue_Id).first()
-    venue_name = venue_name.Name
+    try:
+        event = Event.query.filter_by(EventId=event_id).first()
+        course_id = event.CourseID
+        event_name = event.Name
+        event_address = event.Address
+        venue_name = Venue.query.filter_by(VenueId=event.Venue_Id).first()
+        venue_name = venue_name.Name
 
-    all_players = PlayerDivision.query.filter_by(Event_Id=event_id).all()
-    all_divisions = Division.query.filter_by(EventID=event_id).distinct().all()
+        page_title = event_name + ' - ' + venue_name + ' @ ' + event_address
 
-    page_info = {}
-    score_totals = {}
-    for divisions in all_divisions:
-        page_info[divisions.DivisionID] = {}
+        all_players = PlayerDivision.query.filter_by(Event_Id=event_id).all()
+        all_divisions = Division.query.filter_by(EventID=event_id).distinct().all()
 
-    for player in all_players:
-        player_name = Player.query.filter_by(PlayerId=player.Player_Id).first()
-        player_name = player_name.Name
-        player_scores = Score.query.filter_by(PlayerID=player.Player_Id, EventID=event_id).all()
+        page_info = {}
+        score_totals = {}
+        for divisions in all_divisions:
+            page_info[divisions.DivisionID] = {}
 
-        page_info[player.Division_Id].update({player.Player_Id:{}})
-        page_info[player.Division_Id][player.Player_Id].update({"Name":player_name})
-        page_info[player.Division_Id][player.Player_Id].update({"Scores":{}})
+        for player in all_players:
+            player_name = Player.query.filter_by(PlayerId=player.Player_Id).first()
+            player_name = player_name.Name
+            player_scores = Score.query.filter_by(PlayerID=player.Player_Id, EventID=event_id).all()
 
-        for score in player_scores:
-            page_info[player.Division_Id][player.Player_Id]["Scores"].update({score.HoleID : score.Strokes})
+            page_info[player.Division_Id].update({player.Player_Id:{}})
+            page_info[player.Division_Id][player.Player_Id].update({"Name":player_name})
+            page_info[player.Division_Id][player.Player_Id].update({"Scores":{}})
 
-        score_totals.update({player.Player_Id : sum(page_info[player.Division_Id][player.Player_Id]["Scores"].values())})
+            for score in player_scores:
+                page_info[player.Division_Id][player.Player_Id]["Scores"].update({score.HoleID : score.Strokes})
 
-    holes = Hole.query.filter_by(CourseID=course_id).order_by(Hole.Hole_Num.asc())
+            score_totals.update({player.Player_Id : sum(page_info[player.Division_Id][player.Player_Id]["Scores"].values())})
+
+        holes = Hole.query.filter_by(CourseID=course_id).order_by(Hole.Hole_Num.asc())
+        
+        num_of_holes = 0
+        for entry in holes:
+            num_of_holes += 1
+
+        par_list = []
+        for hole in range(0,num_of_holes):
+            par_list.append(holes[hole].Par)
+        par_total = sum(par_list)
     
-    num_of_holes = 0
-    for entry in holes:
-        num_of_holes += 1
-
-    par_list = []
-    for hole in range(0,num_of_holes):
-        par_list.append(holes[hole].Par)
-    par_total = sum(par_list)
-
     # TODO: Player Results
 
-    return render_template(
-        "/scores/scores.html", 
-        event_name = event_name,
-        event_address = event_address,
-        venue_name = venue_name,
-        holes = holes, 
-        page_info = page_info, 
-        num_of_holes = num_of_holes, 
-        par_list = par_list, 
-        par_total = par_total,
-        score_totals = score_totals
-    )
+        return render_template(
+            "/scores/scores.html", 
+            page_title=page_title,
+            holes = holes, 
+            page_info = page_info, 
+            num_of_holes = num_of_holes, 
+            par_list = par_list, 
+            par_total = par_total,
+            score_totals = score_totals
+        )
+    except:
+        return render_template(
+            "/scores/scores.html", 
+            page_title = "No Event Found",
+        )
 
 @app.route('/scores/home', methods=['POST','GET'])
 def editscores_landing():
